852c24a0ed604870bb2b0b960e75dd6abc1c6341
rbibler <rbibler01@hotmail.com>
2016-08-22 08:26:41 -0400

    Added the mixer
    Still need to do the actual Java audio work.

---
+++ src/com/bibler/awesome/bibnes/systems/Mixer.java
@@ -0,0 +1,9 @@
+package com.bibler.awesome.bibnes.systems;
+
+public class Mixer {
+	
+	public void output(double pulseOneValue, double pulseTwoValue, double triValue, double noiseValue, double DMCValue) {
+		//System.out.println("Mixed!: " + "\n     Pulse 1: " + pulseOneValue + "\n     Pulse 2: " + pulseTwoValue);
+	}
+
+}
