47e4cfa4066b2c47aa68cc55f38901790cd61958
rbibler <rbibler01@hotmail.com>
2016-09-06 09:28:43 -0700

    Tweaked Mapper 2 a bit
    Still having a problem with sprites though.

--- src/com/bibler/awesome/bibnes/mappers/Mapper002.java
+++ src/com/bibler/awesome/bibnes/mappers/Mapper002.java
@@ -2,31 +2,31 @@ package com.bibler.awesome.bibnes.mappers;
 
 public class Mapper002 extends Mapper {
 	
-	private int bankSelect;
+	private int prgBank1;
+	private int prgBank2;
 	private int numBanks;
-	private int lastBankStart;
 	
 	@Override
 	public void setPrgMemSize(int size) {
 		super.setPrgMemSize(size);
 		numBanks = size / 0x4000;
-		lastBankStart = size - 0x4000;
+		prgBank2 = numBanks - 1;
 	}
 	
 	@Override
 	public void writePrg(int address, int data) {
 		if(address >= 0x8000 && address <= 0xFFFF) {
-			bankSelect = data;
+			prgBank1 = data % numBanks;
 		}
 	}
 	
 	@Override
 	public int readPrg(int address) {
 		if(address >= 0xC000 && address <= 0xFFFF) {
-			return prgMem[lastBankStart + (address - 0xC000)];
+			return prgMem[(prgBank2 * 0x4000) + (address - 0xC000)];
 		} else {
 			final int offset = address - 0x8000;
-			return prgMem[(bankSelect * 0x4000) + offset];
+			return prgMem[(prgBank1 * 0x4000) + offset];
 		}
 	}
 	
